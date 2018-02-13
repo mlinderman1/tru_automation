2592ce119cec63b9690d25bb72a2abca0cfbba30
2201v <surendra.pericherla@toysrus.com>
2018-02-13 14:26:16 -0500

    These files are for enviroment settings

---
+++ src/main/java/com/toysrus/automation/utils/LocatorProps.java
@@ -0,0 +1,49 @@
+package com.toysrus.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class LocatorProps {
+	
+
+	static InputStream inputStream;
+
+	public static String getProperty(String key)  {
+
+		Properties properties = new Properties();
+		
+		if (ConfigProps.getProperty("env").equals("qa")) {
+
+			try {
+				inputStream = new FileInputStream("qa.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				inputStream = new FileInputStream("prod.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		
+		// load a properties file
+		try {
+			properties.load(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return properties.getProperty(key);
+
+	}
+
+}

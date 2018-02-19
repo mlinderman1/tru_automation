1c951d0bb0f0ba0a08e6047736b88464b2597a18
2201v <2201v@172.16.14.117>
2018-02-19 10:46:28 -0500

    Added Report files for Reports functionality

--- src/test/java/com/toysrus/automation/TestCase.java
+++ src/test/java/com/toysrus/automation/TestCase.java
@@ -1,14 +1,29 @@
 package com.toysrus.automation;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
+import cucumber.api.java.After;
 import cucumber.api.junit.Cucumber;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features/"},
+@CucumberOptions(features = {"src/test/resources/features/Webstore_AddToCart.feature"},
         glue="com.toysrus.automation.steps",
+        		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
         strict = false)
 
 public class TestCase {
+	
+	@AfterClass
+    public static void setup() {
+        Reporter.loadXMLConfig(new File("/extent-config.xml"));
+        Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+	    Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
 
-}
+ }
+}	
+	
